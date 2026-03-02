6f29e9deed94f86d1723187735d698b4192f352d
divyalonare <divya.lonare.cse@ghru.edu.in.>
2026-03-02 23:43:27 +0530

    file handle

--- file.js
+++ file.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
 
-fs.writeFileSync('./test.txt',"test data");
+fs.writeFile('./test.txt',"test data asyn" , (err) => {});
 
+const result = fs.readFileSync('./details.txt', 'utf-8');
+console.log(result);
+
+fs.appendFileSync('./test.txt', "\nappended data sync ");
+const logEntry = `Log entry at: ${new Date().toLocaleString()}\n`;
+fs.appendFileSync("./test.txt", logEntry);
+
+console.log("Date has been logged!");
