e7b41d978f797ea4055172fcbcc11eef98d6eaf2
divyalonare <divya.lonare.cse@ghru.edu.in.>
2026-03-04 22:45:49 +0530

    file handle

--- file.js
+++ file.js
@@ -10,3 +10,9 @@ const logEntry = `Log entry at: ${new Date().toLocaleString()}\n`;
 fs.appendFileSync("./test.txt", logEntry);
 
 console.log("Date has been logged!");
+fs.appendFileSync("./test.txt", `${Date.now()} hello\n`);
+fs.cpSync('./test.txt', './test_copy.txt');
+
+fs.unlinkSync('./test_copy.txt');
+console.log(fs.statSync('./test.txt'));
+fs.mkdirSync('.myDir/ab/cd', { recursive: true });
